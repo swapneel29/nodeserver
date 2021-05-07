a43b6ce1cae69d5d0f99eb169c478352f8bcd743
swapneel29 <swapneel1098@gmail.com>
2021-05-08 00:32:32 +0530

    first noderserver

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('I am learning Devops\n');
+  res.send('I am learning git\n');
 });
 
 app.listen(PORT, HOST);

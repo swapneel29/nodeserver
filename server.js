1bd0ea47b5e4a0e0be2363699276d3f9c305d614
swapneel29 <swapneel1098@gmail.com>
2021-05-08 01:13:34 +0530

    second commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('I am learning git\n');
+  res.send('I love you baby\n');
 });
 
 app.listen(PORT, HOST);
